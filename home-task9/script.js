dc0b82cd6932c8602066b14947a422d5e30da3ef
Slava-dh <slevko88@gmail.com>
2021-05-19 20:07:46 +0300

    commit

---
+++ home-task9/script.js
@@ -0,0 +1,11 @@
+
+    let listEl = document.querySelector('ul');
+    let inputEl = document.getElementById("input-text");
+    let pushBtn = document.getElementById("push-btn");
+
+    pushBtn.onclick = () => {
+        let listLiEl = document.createElement("li");
+        listLiEl.innerText = inputEl.value;
+        listEl.appendChild(listLiEl);
+        inputEl.value = "";
+    };
