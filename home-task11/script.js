5c26b21580db77c7bf1f6d0498de754a7596c4af
Sjva <slevko88@gmail.com>
2021-06-02 20:01:16 +0300

    little fix HT11

--- home-task11/script.js
+++ home-task11/script.js
@@ -1,64 +1,50 @@
 let listEl = document.querySelector("ul");
 let inputEl = document.getElementById("input-text");
 let pushBtnEl = document.getElementById("push-btn");
+let myDate = new Date();
+const dataItem = `${myDate.getDate()}/${myDate.getMonth() + 1}/${myDate.getFullYear()} ${myDate.getHours()}:${myDate.getMinutes()}`;
 
 function createElementsItem (){
     let listLiEl = document.createElement("li");
+    listLiEl.innerHtml = `
+        <input class="checkbox" type="checkbox">
+        <span class="list-item"></span>
+        <pre class="data"></pre>
+        <i class="fa fa-trash" aria-hidden="true"></i>
+        <i class="fa fa-clone" aria-hidden="true"></i>
+     `;
 
-    // дбавить чекбокс.
-    listLiEl.insertAdjacentHTML("afterBegin", `<input class="checkbox" type="checkbox">`);
-
-    // создал li и добавил в него span  с текстом инпута
-    let listSpanEl = document.createElement("span");
-    listSpanEl.classList.add("list-item");
-    listLiEl.append(listSpanEl);
+    // добавил span в li  с текстом инпута
+    let listSpanEl = document.getElementsByClassName("list-item");
     listSpanEl.innerText = inputEl.value;
 
     // дбавить дату
-    let myDate = new Date();
-    let listDataEl = document.createElement("pre");
-    listDataEl.classList.add("data");
-    listLiEl.append(listDataEl);
-    listDataEl.innerText = `${myDate.getDate()}/${myDate.getMonth() + 1}/${myDate.getFullYear()} ${myDate.getHours()}:${myDate.getMinutes()}`;
-
-    // удалить и копировать.
-    listLiEl.insertAdjacentHTML("beforeEnd", `<i class="fa fa-trash" aria-hidden="true"></i><i class="fa fa-clone" aria-hidden="true"></i>`);
+    let listDataEl = document.getElementsByClassName("data");
+    listDataEl.innerText = dataItem;
 
     listEl.appendChild(listLiEl);
     inputEl.value = "";
 }
 
-
-// клик по странице для старта функций (зачеркнуть, удалить, копировать)
-document.addEventListener("click", (event) => {
+const clickElementItem = event => {
     let element = event.target;
     // запуск функций по клику на кнопку
     if (element === pushBtnEl) {
         if (!inputEl.value) return; //проверка на пуcтой инпут
         createElementsItem ();
-    }
-
-    // клик на чекбокс
-    if (element.type === "checkbox") {
+    } else if (element.type === "checkbox") {      // клик на чекбокс
         if (element.checked) {
             element.nextSibling.classList.add("strikethrough");
         }
         else {
             element.nextSibling.classList.remove("strikethrough");
         }
-    }
-
-    // удаляем строку по клику на корзину
-    if (element.classList.contains('fa-trash')) {
+    }else if (element.classList.contains('fa-trash')) {    // удаляем строку по клику на корзину
         element.parentElement.remove();
-    }
-
-    // копируем li по клику на иконку копирования
-    if (element.classList.contains('fa-clone')) {
+    }else if (element.classList.contains('fa-clone')) {      // копируем li по клику на иконку копирования
         let cloneItem = element.parentElement.cloneNode(true);
-        let copyData = new Date();
         element.parentElement.parentElement.append(cloneItem);
-        cloneItem.childNodes[2].innerText = `${copyData.getDate()}/${copyData.getMonth() + 1}/${copyData.getFullYear()} ${copyData.getHours()}:${copyData.getMinutes()}`;
+        cloneItem.childNodes[2].replace = (dataItem, dataItem);
     }
-});
-
+};
+listEl.addEventListener('click', clickElementItem);
